f378a358faaf7ae361efc358c8444a86f25165fc
abdelmoula213 <154276340+abdelmoula213@users.noreply.github.com>
2026-03-14 21:05:41 +0000

    Update SaharaDesertTour.tsx

--- src/pages/SaharaDesertTour.tsx
+++ src/pages/SaharaDesertTour.tsx
@@ -189,7 +189,7 @@ const SaharaDesertTour = () => {
       </div>
 
       <BookingCalendarWidget
-        bokunWidgetSrc="https://widgets.bokun.io/online-sales/6a2b0f05-7d88-47b8-ac01-19cb2d2e3062/experience-calendar/1174802"
+        bokunWidgetSrc=""https://widgets.bokun.io/online-sales/33e3b4d4-1dd7-4cd5-912d-61a588e6f5a3/experience-calendar/1174802"
       />
     </>
   );
