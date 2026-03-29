a924af2f49060c78f537202486b8b9f4a4327e65
abdelmoula213 <154276340+abdelmoula213@users.noreply.github.com>
2026-03-29 19:43:09 +0100

    Remove logo image from EmbedSocialWidget
    Removed the EmbedSocial logo image from the widget.

--- src/components/EmbedSocialWidget.tsx
+++ src/components/EmbedSocialWidget.tsx
@@ -16,7 +16,6 @@ const EmbedSocialWidget = () => {
   return (
     <div className="embedsocial-hashtag" data-ref="c9f9de6e323815e0237dc7dd231126ed0cc9eb15" data-dynamicload="yes" data-lazyload="yes">
       <a className="feed-powered-by-es feed-powered-by-es-feed-img es-widget-branding" href="https://embedsocial.com/google-reviews-widget/" target="_blank" title="Embed Google reviews">
-        <img src="https://embedsocial.com/cdn/icon/embedsocial-logo.webp" alt="EmbedSocial" />
       </a>
     </div>
   );
